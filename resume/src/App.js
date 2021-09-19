2712ebd44fb78e797c61d8a58af582bf566ff70a
Anindya Biswas <anindya33046@gmail.com>
2021-09-20 04:16:50 +0600

    same as tutorial

--- resume/src/App.js
+++ resume/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './components/header/header';
+import About from './components/about/about';
+import Resume from './components/resume/resume';
+import Portfolio from './components/portfolio/portfolio';
+import Testimonials from  './components/testimonials/testimonials';
+import ContactUs from './components/contactus/contactus';
+import Footer from './components/footer/footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <About />
+        <Resume />
+        <Portfolio />
+        <Testimonials />
+        <ContactUs />
+        <Footer />
+      </div>
+    );
+  }
 }
-
 export default App;
--- resume/src/components/footer/footer.js
+++ resume/src/components/footer/footer.js
@@ -23,7 +23,7 @@ export default class Footer extends Component {
           <a className="smoothscroll" title="Back to Top" href="#top">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M6 4h12v2H6zm5 10v6h2v-6h5l-6-6-6 6z" /></svg>
           </a>
-        </div> {/* end ss-go-top */}
+        </div> 
       </footer>
       </React.Fragment>
     );
