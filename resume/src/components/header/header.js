5c42d069a5550452dae27f98474c4b81cc28b192
Anindya Biswas <anindya33046@gmail.com>
2021-09-20 03:02:32 +0600

    move header to component

---
+++ resume/src/components/header/header.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+export default class Header extends Component {
+  render() {
+    return (
+      <React.Fragment>
+      {
+      var NewComponent = React.createClass({
+        render: function() {
+          return (
+
+            <header className="s-header">
+              <div className="row s-header__nav-wrap">
+                <nav className="s-header__nav">
+                  <ul>
+                    <li className="current"><a className="smoothscroll" href="#hero">Home</a></li>
+                    <li><a className="smoothscroll" href="#about">About</a></li>
+                    <li><a className="smoothscroll" href="#resume">Resume</a></li>
+                    <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+                    <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+                    <li><a className="smoothscroll" href="#contact">Say Hello</a></li>
+                  </ul>
+                </nav>
+              </div> {/* end row */}
+              <a className="s-header__menu-toggle" href="#0" title="Menu">
+                <span className="s-header__menu-icon" />
+              </a>
+            </header>
+          );
+        }
+      });
+      }
+      <header id="home">
+        ....
+      </header>
+      </React.Fragment>
+    );
+  }
+}
