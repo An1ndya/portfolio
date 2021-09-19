3674c2306010090d986eff9d50f114173944d193
Anindya Biswas <anindya33046@gmail.com>
2021-09-20 02:58:40 +0600

    Create header.js
    The converter will remove extra comments, add appropriate closing tags, and indent the code accordingly. Next, create a new class in header.js file in resume=>src=>header folder and paste the generated code in render function as follows:

---
+++ resume/src/header/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+export default class Header extends Component {
+  render() {
+    return (
+      <React.Fragment>
+      {/*generated code*/}
+      <header id="home">
+        ....
+      </header>
+      </React.Fragment>
+    );
+  }
+}
